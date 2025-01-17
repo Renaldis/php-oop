38c598abf75de0fd35f4ed5822869ea8500e7f4c
Renaldis <renaldis142@gmail.com>
2025-01-17 17:52:33 +0700

    inheritance-class

---
+++ chapter2/inheritance-class.php
@@ -0,0 +1,32 @@
+<?php 
+
+class User
+{
+    var $firstName;
+    var $lastName;
+    var $userName;
+
+    function fullName()
+    {
+        return $this->firstName . " " . $this->lastName;
+    }
+}
+
+class Customer extends User
+{
+
+}
+
+$c = new Customer();
+$c->firstName="Customer";
+$c->lastName="Doe";
+
+echo $c->fullName() . "<br>";
+
+if(is_subclass_of($c, "User")){
+    echo "Instance Customer merupakan subclass Class User <br />";
+}
+
+$parents = class_parents($c);
+echo implode(", ", $parents);
+?>
